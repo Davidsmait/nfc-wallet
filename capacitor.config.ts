6248fc8c34ecc90a68ce83c9b6a40a4f29173983
davidsan <david.sanluis@qrsof.com>
2024-03-28 12:32:25 -0600

    fix ios

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'com.nfc.wallet',
   appName: 'nfc-wallet',
   webDir: 'www',
   server: {
